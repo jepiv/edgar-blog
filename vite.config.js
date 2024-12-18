fe0a40a5a468eabbf132c3e46abef2cb8ca984f9
Joseph Parker IV <joeparkeriv@gmail.com>
2024-12-17 21:44:56 -0500

    more page stuff

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,6 @@ export default defineConfig({
   plugins: [react()],
   base: '/EdgarBlog/',
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-      },
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
+    outDir: 'dist'
   }
 })
