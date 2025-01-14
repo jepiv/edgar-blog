a6272d37ccfb28922482bb182987e58e08a49e84
Joseph Parker IV <joeparkeriv@gmail.com>
2025-01-14 15:29:27 -0500

    date

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="min-h-screen bg-[#0d0d0d] py-12 px-4">
       {/* Big page title */}
       <p className="text-gray-300 max-w-3xl mx-auto text-lg italic opacity-80 justify-left">
-      January 15, 2025
+      January 14, 2025
       </p>
       <div className="my-2"></div>
       <h1 className="page-title">
